a5eba8d0f0c0fb17b3318bb40d87ee5b4eee5604
BishwaJit07 <bishwajitr50@gmail.com>
2023-04-09 21:19:46 +0600

    tailwind added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,30 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  themes: [
+    {
+      mytheme: {
+      
+"primary": "#d8635d",
+      
+"secondary": "#38147f",
+      
+"accent": "#a1f4c1",
+      
+"neutral": "#1F282E",
+      
+"base-100": "#242F42",
+      
+"info": "#9DAEEC",
+      
+"success": "#3BE39D",
+      
+"warning": "#956B09",
+      
+"error": "#F73126",
+      },
+    },
+  ],
+  plugins: [require("daisyui")],
 }
 
